55f1ae1c2d197dea4342367fc52dfa362d182f55
nitram509 <maki@bitkings.de>
2025-03-11 11:36:52 +0100

    change: `today()` just returns the date

---
+++ builtins_misc_test.go
@@ -0,0 +1,18 @@
+package feel
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Test_builtin_misc_today_just_returns_a_date(t *testing.T) {
+	actual, err := EvalString(`today()`)
+	assert.Nil(t, err)
+
+	now := time.Now()
+	date := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	expected := FEELDate{t: date}
+
+	assert.Equal(t, &expected, actual)
+}
